182857bbd4b571e95997193ae6db265d98a0dfb8
10x developer <gio2004n@gmail.com>
2024-03-31 21:40:12 +0400

    optimized miner, made the whole process allocation free, by writing a new function to convert numeric values to byte arrays

--- src/Domain/Common/Miner.cs
+++ src/Domain/Common/Miner.cs
@@ -4,6 +4,19 @@ namespace Domain.Common;
 
 public static class Miner
 {
+    private static void CopyWithoutAllocating(long value, Span<byte> dest)
+    {
+        for (var i = 0; i < sizeof(long); i++)
+        {
+            dest[i] = (byte)((value >> 8 * i) & 0xFF);
+        }
+    }
+
+    private static void CopyWithAllocating(long value, Span<byte> dest)
+    {
+        BitConverter.GetBytes(value).AsSpan().CopyTo(dest);
+    }
+
     public static long Mine(byte[] data, int difficulty)
     {
         int destOffset = data.Length - sizeof(long);
@@ -12,14 +25,16 @@ public static class Miner
         ReadOnlySpan<byte> predicate = stackalloc byte[halfDifficulty];
         Span<byte> payload = data;
 
-        Span<byte> nonceBuffer = stackalloc byte[sizeof(long)];
+        Span<byte> nonceBuffer = stackalloc byte[8];
+        Span<byte> nonceBufferA = stackalloc byte[8];
         Span<byte> hashBuffer = stackalloc byte[32];
 
         long nonce = 0;
 
         while (true)
         {
-            nonceBuffer = BitConverter.GetBytes(nonce).AsSpan();
+            CopyWithoutAllocating(nonce, nonceBuffer);
+
             nonceBuffer.CopyTo(payload.Slice(destOffset, sizeof(long)));
             SHA256.HashData(payload, hashBuffer);
 
