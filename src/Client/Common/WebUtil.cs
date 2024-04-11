b7be42b3dd31581d202fd3dc2a1097f91b649254
10x developer <gio2004n@gmail.com>
2024-04-11 21:48:08 +0400

    reformat components

---
+++ src/Client/Common/WebUtil.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+namespace Client.Common;
+
+public static class WebUtil
+{
+    public static int GetGradientDegrees(string address) => int.Parse(address[2..8], NumberStyles.HexNumber) * 2047 % 360;
+    public static int GetGradientDegrees(long index) => (int)(index * 2047 % 360);
+}
