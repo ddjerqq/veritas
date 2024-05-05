aabc2f3168b1f2ade5bae3617bdbfed8d085f0b8
10x developer <gio2004n@gmail.com>
2024-05-06 03:11:25 +0400

    start adding /blockchain page. made the table for the blocks

--- src/Application/Dto/BlockDto.cs
+++ src/Application/Dto/BlockDto.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using Domain.Common;
 using Domain.Entities;
 using Domain.ValueObjects;
 
@@ -29,7 +30,7 @@ public record BlockDto()
 
     public List<VoteDto> Votes { get; } = [];
 
-    public string ShortHash => $"{Hash[..4]}-{Hash[^4..]}";
+    public string ShortHash => $"{Hash[..8]}-{Hash[^8..]}";
 
     public string ShortPreviousHash => $"{PreviousHash[..4]}-{PreviousHash[^4..]}";
 
@@ -50,4 +51,17 @@ public record BlockDto()
         })
         .MaxBy(partyCounts => partyCounts.Count)?
         .Party;
+
+    public static BlockDto RandomBlockDto(long? index = null, int? votes = null)
+    {
+        return new BlockDto(
+            index ?? Random.Shared.Next(0, 10_000_000),
+            Random.Shared.NextInt64(0, 10_000_000),
+            StringExt.RandomHexString(64),
+            StringExt.RandomHexString(64),
+            StringExt.RandomHexString(64),
+            Enumerable.Range(0, Random.Shared.Next(1, votes ?? 100))
+                .Select(_ => VoteDto.RandomVoteDto())
+                .ToList());
+    }
 }
