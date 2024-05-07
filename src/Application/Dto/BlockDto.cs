c177b83f132d8123c1a12db85118f1a78fd4c530
10x developer <gio2004n@gmail.com>
2024-05-07 16:01:30 +0400

    removed random

--- src/Application/Dto/BlockDto.cs
+++ src/Application/Dto/BlockDto.cs
@@ -49,17 +49,4 @@ public record BlockDto()
         })
         .MaxBy(partyCounts => partyCounts.Count)?
         .Party;
-
-    public static BlockDto RandomBlockDto(long? index = null, int? votes = null)
-    {
-        return new BlockDto(
-            index ?? Random.Shared.Next(0, 10_000_000),
-            Random.Shared.NextInt64(0, 10_000_000),
-            StringExt.RandomHexString(64),
-            StringExt.RandomHexString(64),
-            StringExt.RandomHexString(64),
-            Enumerable.Range(0, Random.Shared.Next(1, votes ?? 100))
-                .Select(_ => VoteDto.RandomVoteDto())
-                .ToList());
-    }
 }
--- src/Application/Dto/VoteDto.cs
+++ src/Application/Dto/VoteDto.cs
@@ -13,17 +13,4 @@ public record VoteDto(string Hash, long Nonce, DateTime Timestamp, int PartyId, 
     public string ShortHash => $"{Hash[..4]}-{Hash[^4..]}";
 
     public string ShortVoterAddress => VoterAddress[..8];
-
-    public static VoteDto RandomVoteDto()
-    {
-        var party = Random.Shared.GetItems(Party.All.Select(p => (int)p).ToArray(), 1)[0];
-
-        return new VoteDto(
-            StringExt.RandomHexString(64),
-            Random.Shared.Next(0, 10_000_000),
-            DateTime.Now,
-            party,
-            "0x" + StringExt.RandomHexString(42),
-            null);
-    }
 }
